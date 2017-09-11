45aa47e7125774538f9c6f1236bf5e3a5a44bf73
Ibrahim Sowunmi <i.sowunmi97@gmail.com>
2017-09-11 23:02:39 +0100

    Failed to complete exercise 33

---
+++ 2013-OOProgrammingWithJava-PART2/week10-week10_33.Dungeon/src/dungeon/Dungeon.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dungeon;
+import java.util.*;
+
+/**
+ *
+ * @author ibz
+ */
+public class Dungeon {
+    private int length;
+    private int height;
+    private int vampires;
+    private int moves;
+    private boolean vampiresMove;
+    
+    public Dungeon(int length, int height, int vampires, int moves, boolean vampiresMove) {
+       this.length = length;
+       this.height = height;
+       this.vampires = vampires;
+       this.moves = moves;
+       this.vampiresMove = vampiresMove;
+        
+    }
+    
+    
+    
+    
+    
+    
+}
--- 2013-OOProgrammingWithJava-PART2/week10-week10_33.Dungeon/src/dungeon/Main.java
+++ 2013-OOProgrammingWithJava-PART2/week10-week10_33.Dungeon/src/dungeon/Main.java
@@ -4,5 +4,8 @@ package dungeon;
 public class Main {
     public static void main(String[] args) {
         //new Dungeon(10,10,5,14,true).run();
+        Dungeon castle = new Dungeon(3,4,5,5,true);
+        
+
     }
 }
