5a792ff9008f7e021c3d8622c0fc61558ef67441
Ibrahim Sowunmi <i.sowunmi97@gmail.com>
2017-09-14 19:54:25 +0100

    Completed exercise 37

--- 2013-OOProgrammingWithJava-PART2/week11-week11_37.Survey/src/survey/UserInterface.java
+++ 2013-OOProgrammingWithJava-PART2/week11-week11_37.Survey/src/survey/UserInterface.java
@@ -1,5 +1,6 @@
 package survey;
 
+import java.awt.Color;
 import java.awt.Container;
 import java.awt.Dimension;
 import javax.swing.*;
@@ -11,6 +12,55 @@ public class UserInterface implements Runnable {
     @Override
     public void run() {
         // Create your app here
+        frame = new JFrame("Survey");
+        frame.setPreferredSize(new Dimension(200, 300));
+        
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        
+        createComponents(frame.getContentPane());
+        
+        frame.pack();
+        frame.setVisible(true);
+    }
+    
+    public void createComponents(Container container) {
+        BoxLayout layout = new BoxLayout(container, BoxLayout.Y_AXIS);
+        container.setLayout(layout);
+        
+        container.add(new JLabel("Are you?"));
+        
+        JCheckBox yesBox = new JCheckBox("Yes!");
+        JCheckBox noBox = new JCheckBox("No!");
+        
+        container.add(yesBox);
+        container.add(noBox);
+        
+        ButtonGroup buttonGroup2 = new ButtonGroup();
+        
+        buttonGroup2.add(yesBox);
+        buttonGroup2.add(noBox);
+        container.add(yesBox);
+        container.add(noBox);
+        
+        container.add(new JLabel("Why?"));
+        
+        JRadioButton radioButton1 = new JRadioButton("No reason.");
+        JRadioButton radioButton2 = new JRadioButton("Because it is fun!");
+        
+        ButtonGroup buttonGroup1 = new ButtonGroup();
+        
+        buttonGroup1.add(radioButton1);
+        buttonGroup1.add(radioButton2);
+        container.add(radioButton1);
+        container.add(radioButton2);
+        
+        container.add(new JButton("Done!"));
+        
+        
+        
+        
+        
+        
     }
 
 
